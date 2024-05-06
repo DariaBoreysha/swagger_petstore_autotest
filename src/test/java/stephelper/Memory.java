3798fcb15ebfb806e73732c31b3d8e207d3a73b3
Bareisha Darya <dariaboreysha@gmail.com>
2024-05-06 09:04:09 +0300

    added Memory.java

---
+++ src/test/java/stephelper/Memory.java
@@ -0,0 +1,34 @@
+package stephelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Memory {
+
+    private static Memory INSTANCE;
+    private static final Map<String, String> map = new HashMap<>();
+
+    public static void getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Memory();
+        }
+    }
+
+    public static void put(
+            String key,
+            String value
+    ) {
+        map.put(key, value);
+    }
+
+    public static String get(
+            String key
+    ) {
+        return map.get(key);
+    }
+
+    public static void clear() {
+
+        map.clear();
+    }
+}
