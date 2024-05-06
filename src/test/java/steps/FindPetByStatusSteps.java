1929fdecd9a2809d1318f1d179b366e7f560e59e
Bareisha Darya <dariaboreysha@gmail.com>
2024-05-06 09:05:52 +0300

    edited step in steps class

--- src/test/java/steps/FindPetByStatusSteps.java
+++ src/test/java/steps/FindPetByStatusSteps.java
@@ -1,24 +1,33 @@
 package steps;
 
 import client.HttpClient;
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.When;
+import stephelper.Memory;
+
 import java.util.Map;
 
 public class FindPetByStatusSteps {
 
-  @When("формируем GET запрос и отправляем на {string} с одним параметром {string} и получаем тело JSON")
-  public void createGetRequest(
-      String url,
-      String statusValue,
-      DataTable table
+  ObjectMapper objectMapper = new ObjectMapper();
+
+  @When("формируем GET запрос с одним валидным параметром {string} со значением {string}, отправляем на {string} и сохраняем ответ в Memory как {string}")
+  public void sendGetRequest(
+          String parameterName,
+          String parameterValue,
+          String url,
+          String memoryVariableName,
+          DataTable table
   ) {
-    HttpClient httpClient = new HttpClient(url);
+    HttpClient httpClient = new HttpClient();
     Map<String, String> headers = table.transpose().asMap();
-    String responseBody = httpClient.get(
-        headers,
-        "status",
-        statusValue
+    String responseBody = httpClient.sendGetRequest(
+            url,
+            headers,
+            parameterName,
+            parameterValue
     );
+    Memory.put(memoryVariableName, responseBody);
   }
 }
