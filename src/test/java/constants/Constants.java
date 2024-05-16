5d43852ac9ddb08941723aee7c98517863bb7e94
Bareisha Darya <dariaboreysha@gmail.com>
2024-05-16 12:45:17 +0300

    deleted unused constants

--- src/test/java/constants/Constants.java
+++ src/test/java/constants/Constants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Constants {
 
-    public static final String ROOT = "." + File.separator + "src" + File.separator + "test" + File.separator;
-    public static final String RESOURCES = ROOT + "resources" + File.separator;
+    public static final String RESOURCES = "." + File.separator + "src" + File.separator
+            + "test" + File.separator + "resources" + File.separator;
     public static final String SCHEMA_FOLDER = RESOURCES + File.separator + "json_schema" + File.separator;
 }
