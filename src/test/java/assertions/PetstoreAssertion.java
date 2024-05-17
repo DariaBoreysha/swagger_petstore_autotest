a0e57349cdf3f540e8d28b5036928916743c3e42
Bareisha Darya <dariaboreysha@gmail.com>
2024-05-17 14:37:08 +0300

    added TODO for the future

--- src/test/java/assertions/PetstoreAssertion.java
+++ src/test/java/assertions/PetstoreAssertion.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PetstoreAssertion {
 
+    //TODO Отрефакторить в случае, если необходимо будет проверять не только поле status
     public static void assertBodyStatusFieldValueIsCorrect(
             JsonNode jsonBody,
             String[] expectedValues
