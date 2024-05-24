b06a17e3ef23333a2d2c9c8401e53e5d93e25c44
Bareisha Darya <dariaboreysha@gmail.com>
2024-05-24 13:07:03 +0300

    added file util class

---
+++ src/test/java/utils/FileUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUtil {
+
+    public static String readFileToString(String filePath) {
+        String body;
+        try {
+            body = FileUtils.readFileToString(new File(filePath), "UTF-8");
+        } catch (IOException e) {
+            throw new AtFileUtilException(e);
+        }
+        return body;
+    }
+}
