b0508c8226398501e1746c81b311d1e1be8d3cce
Darya Bareisha <dariaboreysha@gmail.com>
2024-07-25 17:50:29 +0300

    added method realization

--- src/test/java/steps/UpdatePetSteps.java
+++ src/test/java/steps/UpdatePetSteps.java
@@ -1,8 +1,19 @@
 package steps;
 
 import io.cucumber.java.en.And;
+import org.apache.http.HttpResponse;
+import stephelper.Memory;
 
-public class UpdatePetSteps {
+public class UpdatePetSteps extends BaseSteps {
 
-    @And("отправляем PUT запрос c телом из Memory: \"request_body\" на \"https://petstore.swagger.io/v2/pet\" и сохраняем ответ в Memory как \"response_entity\"")
+    @And("отправляем PUT запрос c телом из Memory: {string} на {string} и сохраняем ответ в Memory как {string}")
+    public void sendPutRequest(
+            String memoryRequestKey,
+            String url,
+            String memoryResponseKey
+    ) {
+        String requestBody = Memory.asString(memoryRequestKey);
+        HttpResponse response = httpClient.methodPut().sendRequest(url, requestBody);
+        Memory.put(memoryResponseKey, response);
+    }
 }
